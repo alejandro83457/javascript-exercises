99cccacf31bb9ea02e4f8c34df8c0929e5ad15df
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-19 19:15:39 -0800

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().split("");
+    str = str.filter((ch) => {
+        let code = ch.charCodeAt();
+        // check ASCII values for alphanumeric characters
+        if ((code > 96 && code < 123) || (code > 47 && code < 58)) {
+            return true;
+        } else false;
+    });
+    console.log(str);
+    return str.toString() == str.reverse().toString();
 };
 
 // Do not edit below this line
