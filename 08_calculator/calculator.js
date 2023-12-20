62b0aa8be70b49cbc280a96ea98c66fa6dc1c07b
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-19 18:45:11 -0800

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (vals) {
+    let total = 0;
+    for (let el of vals) total += el;
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function (vals) {
+    let total = 1;
+    for (let el of vals) total *= el;
+    return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    if (a == 0) return 1;
+    return a * factorial(a - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
