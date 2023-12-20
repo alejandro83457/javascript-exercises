20425d425e460f65d2f7d71d140a098b830bf661
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-19 21:12:58 -0800

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    console.log(Number(num));
+    if (Number(num) == NaN || num < 0) return "OOPS";
+    if (num == 0) return 0;
+    let vals = [1, 1];
+    num = Number(num);
+    for (let i = 0; i < num - 2; i++) {
+        vals.push(vals[i] + vals[i + 1]);
+    }
+    return vals[num - 1];
 };
 
 // Do not edit below this line
