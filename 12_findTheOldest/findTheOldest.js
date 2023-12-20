07806bf0901a02e5555c2623d4be503e66dac69b
alejandro83457 <martinezalejandro2001@hotmail.com>
2023-12-20 02:44:48 -0800

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let sorted = people.sort((a, b) => {
+        let age1, age2;
+        if ("yearOfDeath" in a) {
+            age1 = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            age1 = 2023 - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b) {
+            age2 = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            age2 = 2023 - b.yearOfBirth;
+        }
+        return age1 < age2 ? 1 : -1;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
